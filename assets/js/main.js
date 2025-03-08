76b6f704e583d84ea0d8339b354fb5b5770cde4b
abcancode <chidozieintech@gmail.com>
2025-03-08 11:18:42 +0100

    initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,19 @@
+// const textLines = [
+//   "We create software solutions.",
+//   "We design software solutions.",
+//   "We develop software solutions.",
+//   "We maintain software solutions.",
+// ];
+
+// let currentIndex = 0;
+// const textElement = document.querySelector(".text");
+
+// // Function to update the text
+// function updateText() {
+//   textElement.textContent = textLines[currentIndex];
+//   currentIndex = (currentIndex + 1) % textLines.length; // Cycle through the array
+// }
+
+// // Update text every time the image moves up (based on animation duration)
+// const animationDuration = 4000; // 4 seconds (matches CSS animation duration)
+// setInterval(updateText, animationDuration);
